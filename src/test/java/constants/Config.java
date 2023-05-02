df5093167129d75c3676c874cee62de5a48b538c
Roberto Filho <roberto.siilva@hotmail.com>
2023-05-02 14:21:20 -0300

    removendo config driver

--- src/test/java/constants/Config.java
+++ src/test/java/constants/Config.java
@@ -2,7 +2,7 @@ package constants;
 
 public class Config {
 
-    public static String ENVIRONMENT = System.getProperty("env", "browserstack");
+    public static String ENVIRONMENT = System.getProperty("env", "local");
     public static final String USERNAME = "robertosilva_SJgqTj";
     public static final String ACCESS_KEY = "QtLSMzhtYKHZRXSW7kKJ";
 }
