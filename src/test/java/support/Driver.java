af0cf7e09102b8b5f979a740fc2e0a94a0031fd3
Roberto Filho <roberto.siilva@hotmail.com>
2023-05-02 16:04:49 -0300

    remove bonigarcia

--- src/test/java/support/Driver.java
+++ src/test/java/support/Driver.java
@@ -1,7 +1,6 @@
 package support;
 
 import constants.Path;
-import io.github.bonigarcia.wdm.WebDriverManager;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.chrome.ChromeOptions;
